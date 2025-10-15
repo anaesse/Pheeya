2ae5bc98e6726192807e6692c52b2230245aad11
Sophia Edeki <sophiesse143@gmail.com>
2025-10-15 14:07:12 +0100

    refactor: clean up code structure and remove redundant sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./views/**/*.ejs', './assets/js/**/*.{js,ts,vue}'],
   theme: {
     extend: {
+       fontFamily: {
+        sans: ['"IBM Plex Sans"', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+      },
       colors: {
         coral: {
           DEFAULT: '#FF6B6B',
